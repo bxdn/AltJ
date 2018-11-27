39beb2d75b02bda3e3342eccce3d1cad111c4753
ssmolinski <ssmolinski@dramail.com>
2018-11-27 10:15:43 -0500

    Refactored

--- index.js
+++ index.js
@@ -21,16 +21,66 @@ function fadeAudio(){
     },1)
 }
 
-function circleListener(circ1,circ2){
-    circ1.classList.add('circleOut');
-    circ2.style.display = "block";
-    circ2.classList.add('circleIn');
-}
-
 function reflow(el){
     el.style.animation = 'none';
     el.offsetHeight; /* trigger reflow */
     el.style.animation = null;
+}
+
+function firstTrigger(buttons,cont){
+    buttons[3].classList.add('circleOut');
+    document.getElementById("circle4Aud").play();
+    setTimeout(()=>{
+        for(let i = 0; i < 4; i++){
+            buttons[i] = rmBtnFunction(buttons[i]);
+        }
+        cont.classList.remove('rotate2');
+        reflow(cont);
+        cont.classList.add('rotate');
+    },1000);
+    setTimeout(()=>{
+        for(let i = 0; i < 4; i++){
+            buttons[i].classList.add('circleOut');
+        }
+        buttons[4].style.display = 'block';
+        buttons[4].classList.add('circleIn');
+    },13000);
+}
+
+function secondTrigger(buttons,cont){
+    buttons[9].classList.add('circleOut');
+    document.getElementById("circle10Aud").play();
+    setTimeout(()=>{
+        cont.classList.remove('rotate');
+        reflow(cont);
+        cont.classList.add('rotate');
+        for(let i = 4; i < 10; i++){
+            buttons[i] = rmBtnFunction(buttons[i]);
+        }
+        buttons[0].classList.add('circ1');
+        buttons[1].classList.add('circ1');
+        buttons[2].classList.add('circ2');
+        buttons[3].classList.add('circ2');
+    },500);
+    setTimeout(()=>{
+        for(let i = 0; i < 10; i++){
+            buttons[i].classList.remove('circleOut','circleIn','circ1','circ2');
+            buttons[i].classList.add('fadeOut');
+        }
+        buttons[0].style.top = '12%';
+        buttons[1].style.top = '12%';
+        buttons[2].style.top = '88%';
+        buttons[3].style.top = '88%';
+    },12500);
+}
+
+function rmBtnFunction(el){
+    el.classList.remove('circleIn');
+    el.classList.remove('circleOut');
+    var clearButton = el.cloneNode(true);
+    el.parentNode.replaceChild(clearButton, el);
+    clearButton.classList.add('circleIn');
+    return clearButton
 }
 
 function addListeners(){
@@ -39,75 +89,26 @@ function addListeners(){
         const id = "circle" + (i+1).toString();
         buttons.push(document.getElementById(id));
     }
-    for(let i=0; i<3; i++){
-        const audId = "circle"+(i+1).toString()+"Aud";
-        buttons[i].addEventListener("mouseenter", function(){
-            circleListener(buttons[i],buttons[i+1]);
-            document.getElementById(audId).play();
-        });
-    }
-    for(let i=4; i<9; i++){
-        const audId = "circle"+(i+1).toString()+"Aud";
-        buttons[i].addEventListener("mouseenter", function(){
-            circleListener(buttons[i],buttons[i+1]);
-            document.getElementById(audId).play();
-        });
-    }
-    buttons[3].addEventListener("mouseenter", function(){
-        buttons[3].classList.add('circleOut');
-        document.getElementById("circle4Aud").play();
-        setTimeout(()=>{
-            for(let i = 0; i < 4; i++){
-                buttons[i].classList.remove('circleIn');
-                buttons[i].classList.remove('circleOut');
-                buttons[i].classList.add('circleIn');
-                var clearButton = buttons[i].cloneNode(true);
-                buttons[i].parentNode.replaceChild(clearButton, buttons[i]);
-                buttons[i] = clearButton
-            }
-            mainCont = document.getElementById("mainContainer");
-            mainCont.classList.remove('rotate2');
-            reflow(mainCont);
-            mainCont.classList.add('rotate');
-        },1000);
-        setTimeout(()=>{
-            for(let i = 0; i < 4; i++){
-                buttons[i].classList.remove('circleOut','circleIn');
-                reflow(buttons[i]);
+    for(let i=0; i<9; i++){
+        if(i!=3){
+            const audId = "circle"+(i+1).toString()+"Aud";
+            buttons[i].addEventListener("mouseenter", function(){
                 buttons[i].classList.add('circleOut');
-            }
-            buttons[4].style.display = 'block';
-            buttons[4].classList.add('circleIn');
-        },13000);
-    });
-    buttons[9].addEventListener("mouseenter", function(){
-        buttons[9].classList.add('circleOut');
-        document.getElementById("circle10Aud").play();
-        setTimeout(()=>{
-            mainCont.classList.remove('rotate');
-            reflow(mainCont);
-            mainCont.classList.add('rotate');
-            for(let i = 4; i < 10; i++){
-                buttons[i].classList.remove('circleIn');
-                buttons[i].classList.remove('circleOut');
-                var clearButton = buttons[i].cloneNode(true);
-                buttons[i].parentNode.replaceChild(clearButton, buttons[i]);
-                buttons[i] = clearButton
-                buttons[i].classList.add('circleIn');
-            }
-            buttons[0].classList.add('circ1');
-            buttons[1].classList.add('circ1');
-            buttons[2].classList.add('circ2');
-            buttons[3].classList.add('circ2');
-        },500);
-    })
+                buttons[i+1].style.display = "block";
+                buttons[i+1].classList.add('circleIn');
+                document.getElementById(audId).play();
+            });
+        }
+    }
+    var mainCont = document.getElementById("mainContainer");
+    buttons[3].addEventListener("mouseenter", function(){firstTrigger(buttons,mainCont)});
+    buttons[9].addEventListener("mouseenter", function(){secondTrigger(buttons,mainCont)});
 }
 
 function introText(){
     const hiText = document.getElementById('hello');
     const letsText = document.getElementById('lets');
     const audioText = document.getElementById('audText');
-    const phones = document.getElementById('phones');
     hiText.style.display = 'flex';
     hiText.classList.add('fadeIn');
     setTimeout(()=>{fadeOut(hiText)},2000);
@@ -122,13 +123,13 @@ function initialize(){
     /*introText();
     setTimeout(()=>{fadeAudio()},12000);
     setTimeout(()=>{
-        document.getElementById('circle1').classList.add('circleIn');
         document.getElementById('circle1').style.display = 'block';
+        document.getElementById('circle1').classList.add('circleIn');
     },13000);*/
     setTimeout(()=>{fadeAudio()},1000);
     setTimeout(()=>{
-        document.getElementById('circle1').classList.add('circleIn');
         document.getElementById('circle1').style.display = 'block';
+        document.getElementById('circle1').classList.add('circleIn');
     },2000);
 }
 
